5a4e49c37cf3ae4db1499f99665faa98f2907af9
Joel Stanley <intractable@gmail.com>
2017-05-29 13:48:23 -0500

    Squash merge from branch `joel-fix-build-issues-and-broken-tests`

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 { mkDerivation, async, base, bytestring, c2hs, clock, containers
 , grpc, managed, optparse-generic, pipes, proto3-suite, proto3-wire
-, QuickCheck, random, safe, sorted-list, stdenv, stm
-, system-filepath, tasty, tasty-hunit, tasty-quickcheck, text, time
-, transformers, turtle, unix, vector
+, QuickCheck, safe, sorted-list, stdenv, stm, system-filepath
+, tasty, tasty-hunit, tasty-quickcheck, text, time, transformers
+, turtle, unix, vector
 }:
 mkDerivation {
   pname = "grpc-haskell";
@@ -12,21 +12,21 @@ mkDerivation {
   isExecutable = true;
   libraryHaskellDepends = [
     async base bytestring clock containers managed pipes proto3-suite
-    proto3-wire safe sorted-list stm tasty tasty-hunit transformers
-    vector
+    proto3-wire safe sorted-list stm tasty tasty-hunit tasty-quickcheck
+    transformers vector
   ];
   librarySystemDepends = [ grpc ];
   libraryToolDepends = [ c2hs ];
   executableHaskellDepends = [
     async base bytestring containers optparse-generic proto3-suite
-    proto3-wire random system-filepath text transformers turtle vector
+    proto3-wire system-filepath text transformers turtle
   ];
   testHaskellDepends = [
     async base bytestring clock containers managed pipes proto3-suite
     QuickCheck safe tasty tasty-hunit tasty-quickcheck text time
     transformers turtle unix
   ];
-  homepage = "https://github.mv.awakenetworks.net/awakenetworks/gRPC-haskell";
+  homepage = "https://github.com/awakenetworks/gRPC-haskell";
   description = "Haskell implementation of gRPC layered on shared C library";
   license = stdenv.lib.licenses.asl20;
 }
