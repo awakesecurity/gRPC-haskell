awakesecurity/gRPC-haskell

commit 3366dde7ab5fa88e12c006845d5a32da9d959467

Connor/security (#68)
* begin security bindings

* secure channel creation

* SSL credentials support

* add client-side ssl support

* ssl test

* read file before passing to channel credentials, free credentials when done creating

* use localhost key/cert for test

* WIP sketch of callbacks for metadata plugins

* conversion from Haskell auth processor to C

* add test for custom server metadata auth processor

* wip auth failure test

* rebase tweak

* working test of custom auth metadata server processor

* improve security docs, clean up

* add unsafe layer client-side auth metadata plugin functionality

* add client config option for custom auth metadata

* WIP client-side metadata auth plugin. Crashing when calling C callback.

* get initial version of client-side metadata plugins working

* replace String with ByteString in a few places, add function for getting AuthProperty

* AuthContext utilities and more documentation

* end-to-end test of client and server auth plugins

* remove redundant tests

* function for parents in unary calls, add deactivated failing test for auth metadata propagation from parent to child

* some cleanup

* tweaks

* more tweaks

* remove unused file

* docs tweak

* consolidate exports

* update protobuf-wire commit

examples/hellos/hellos-client/Main.hs
@@ -110,7 +110,7 @@ doHelloBi c n = do
 
 highlevelMain :: IO ()
 highlevelMain = withGRPC $ \g ->
-  withClient g (ClientConfig "localhost" 50051 []) $ \c -> do
+  withClient g (ClientConfig "localhost" 50051 [] Nothing) $ \c -> do
     let n = 100000
     putStrLn "-------------- HelloSS --------------"
     doHelloSS c n

examples/echo/echo-client/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists   #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds       #-}
 
@@ -22,7 +23,7 @@ addMethod = MethodName "/echo.Add/DoAdd"
 _unregistered c = U.clientRequest c echoMethod 1 "hi" mempty
 
 regMain = withGRPC $ \g ->
-  withClient g (ClientConfig "localhost" 50051 []) $ \c -> do
+  withClient g (ClientConfig "localhost" 50051 [] Nothing) $ \c -> do
   rm <- clientRegisterMethodNormal c echoMethod
   replicateM_ 100000 $ clientRequest c rm 5 "hi" mempty >>= \case
     Left e -> fail $ "Got client error: " ++ show e
@@ -42,7 +43,7 @@ instance Message AddResponse
 -- TODO: Create Network.GRPC.HighLevel.Client w/ request variants
 
 highlevelMain = withGRPC $ \g ->
-    withClient g (ClientConfig "localhost" 50051 []) $ \c -> do
+    withClient g (ClientConfig "localhost" 50051 [] Nothing) $ \c -> do
     rm <- clientRegisterMethodNormal c echoMethod
     rmAdd <- clientRegisterMethodNormal c addMethod
     let oneThread = replicateM_ 10000 $ body c rm rmAdd
@@ -71,4 +72,5 @@ highlevelMain = withGRPC $ \g ->
                     | dec == AddResponse (x + y) -> return ()
                     | otherwise -> fail $ "Got wrong add answer: " ++ show dec ++ "expected: " ++ show x ++ " + " ++ show y ++ " = " ++ show (x+y)
 
+main :: IO ()
 main = highlevelMain

examples/echo/echo-server/Main.hs
@@ -20,6 +20,7 @@ import qualified Network.GRPC.HighLevel.Server.Unregistered as U
 import           Network.GRPC.LowLevel
 import qualified Network.GRPC.LowLevel.Call.Unregistered   as U
 import qualified Network.GRPC.LowLevel.Server.Unregistered as U
+import           Network.GRPC.Unsafe.Security as U
 
 serverMeta :: MetadataMap
 serverMeta = [("test_meta", "test_meta_value")]
@@ -123,4 +124,4 @@ main :: IO ()
 main = highlevelMainUnregistered
 
 defConfig :: ServerConfig
-defConfig = ServerConfig "localhost" 50051 [] [] [] [] []
+defConfig = ServerConfig "localhost" 50051 [] [] [] [] [] Nothing

examples/hellos/hellos-server/Main.hs
@@ -80,4 +80,4 @@ main :: IO ()
 main = highlevelMainUnregistered
 
 defConfig :: ServerConfig
-defConfig = ServerConfig "localhost" 50051 [] [] [] [] []
+defConfig = ServerConfig "localhost" 50051 [] [] [] [] [] Nothing

cbits/grpc_haskell.c
@@ -1,6 +1,7 @@
 #include <grpc/grpc.h>
 #include <grpc/byte_buffer.h>
 #include <grpc/byte_buffer_reader.h>
+#include <grpc/grpc_security.h>
 #include <grpc/impl/codegen/grpc_types.h>
 #include <grpc/impl/codegen/compression_types.h>
 #include <stdio.h>
@@ -158,7 +159,7 @@ void metadata_array_destroy(grpc_metadata_array **arr){
 }
 
 grpc_metadata* metadata_alloc(size_t n){
-  grpc_metadata *retval = malloc(sizeof(grpc_metadata)*n);
+  grpc_metadata *retval = calloc(n,sizeof(grpc_metadata));
   return retval;
 }
 
@@ -462,4 +463,86 @@ void destroy_arg_array(grpc_arg* args, size_t n){
     }
   }
   free(args);
+}
+
+grpc_auth_property_iterator* grpc_auth_context_property_iterator_(
+  const grpc_auth_context* ctx){
+
+  grpc_auth_property_iterator* i = malloc(sizeof(grpc_auth_property_iterator));
+  *i = grpc_auth_context_property_iterator(ctx);
+  return i;
+}
+
+grpc_server_credentials* ssl_server_credentials_create_internal(
+  const char* pem_root_certs, const char* pem_key, const char* pem_cert,
+  grpc_ssl_client_certificate_request_type force_client_auth){
+
+  grpc_ssl_pem_key_cert_pair pair = {pem_key, pem_cert};
+  grpc_server_credentials* creds = grpc_ssl_server_credentials_create_ex(
+    pem_root_certs, &pair, 1, force_client_auth, NULL);
+  return creds;
+}
+
+grpc_channel_credentials* grpc_ssl_credentials_create_internal(
+  const char* pem_root_certs, const char* pem_key, const char* pem_cert){
+
+  grpc_channel_credentials* creds;
+  if(pem_key && pem_cert){
+    grpc_ssl_pem_key_cert_pair pair = {pem_key, pem_cert};
+    creds = grpc_ssl_credentials_create(pem_root_certs, &pair, NULL);
+  }
+  else{
+    creds = grpc_ssl_credentials_create(pem_root_certs, NULL, NULL);
+  }
+  return creds;
+}
+
+void grpc_server_credentials_set_auth_metadata_processor_(
+  grpc_server_credentials* creds, grpc_auth_metadata_processor* p){
+
+  grpc_server_credentials_set_auth_metadata_processor(creds, *p);
+}
+
+grpc_auth_metadata_processor* mk_auth_metadata_processor(
+  void (*process)(void *state, grpc_auth_context *context,
+                  const grpc_metadata *md, size_t num_md,
+                  grpc_process_auth_metadata_done_cb cb, void *user_data)){
+
+  //TODO: figure out when to free this.
+  grpc_auth_metadata_processor* p = malloc(sizeof(grpc_auth_metadata_processor));
+  p->process = process;
+  p->destroy = NULL;
+  p->state = NULL;
+  return p;
+}
+
+grpc_call_credentials* grpc_metadata_credentials_create_from_plugin_(
+  grpc_metadata_credentials_plugin* plugin){
+
+  return grpc_metadata_credentials_create_from_plugin(*plugin, NULL);
+}
+
+//This is a hack to work around GHC being unable to deal with raw struct params.
+//This callback is registered as the get_metadata callback for the call, and its
+//only job is to cast the void* state pointer to the correct function pointer
+//type and call the Haskell function with it.
+void metadata_dispatcher(void *state, grpc_auth_metadata_context context,
+ grpc_credentials_plugin_metadata_cb cb, void *user_data){
+
+  ((haskell_get_metadata*)state)(&context, cb, user_data);
+}
+
+grpc_metadata_credentials_plugin* mk_metadata_client_plugin(
+  haskell_get_metadata* f){
+
+  //TODO: figure out when to free this.
+  grpc_metadata_credentials_plugin* p =
+    malloc(sizeof(grpc_metadata_credentials_plugin));
+
+  p->get_metadata = metadata_dispatcher;
+  p->destroy = NULL;
+  p->state = f;
+  p->type = "grpc-haskell custom credentials";
+
+  return p;
 }

grpc-haskell.cabal
@@ -55,6 +55,7 @@ library
     Network.GRPC.Unsafe.Metadata
     Network.GRPC.Unsafe.Op
     Network.GRPC.Unsafe
+    Network.GRPC.Unsafe.Security
     Network.GRPC.LowLevel
     Network.GRPC.LowLevel.Server.Unregistered
     Network.GRPC.LowLevel.Client.Unregistered

include/grpc_haskell.h
@@ -2,6 +2,7 @@
 #define GRPC_HASKELL
 
 #include <grpc/grpc.h>
+#include <grpc/grpc_security.h>
 #include <grpc/impl/codegen/slice.h>
 #include <grpc/impl/codegen/time.h>
 #include <grpc/byte_buffer.h>
@@ -163,5 +164,41 @@ void create_int_arg(grpc_arg* args, size_t i,
                     enum supported_arg_key key, int value);
 
 void destroy_arg_array(grpc_arg* args, size_t n);
+
+grpc_auth_property_iterator* grpc_auth_context_property_iterator_(
+  const grpc_auth_context* ctx);
+
+grpc_server_credentials* ssl_server_credentials_create_internal(
+  const char* pem_root_certs, const char* pem_key, const char* pem_cert,
+  grpc_ssl_client_certificate_request_type force_client_auth);
+
+grpc_channel_credentials* grpc_ssl_credentials_create_internal(
+  const char* pem_root_certs, const char* pem_key, const char* pem_cert);
+
+void grpc_server_credentials_set_auth_metadata_processor_(
+  grpc_server_credentials* creds, grpc_auth_metadata_processor* p);
+
+//packs a Haskell server-side auth processor function pointer into the
+//appropriate struct expected by gRPC.
+grpc_auth_metadata_processor* mk_auth_metadata_processor(
+  void (*process)(void *state, grpc_auth_context *context,
+                  const grpc_metadata *md, size_t num_md,
+                  grpc_process_auth_metadata_done_cb cb, void *user_data));
+
+grpc_call_credentials* grpc_metadata_credentials_create_from_plugin_(
+  grpc_metadata_credentials_plugin* plugin);
+
+//type of the callback used to create auth metadata on the client
+typedef void (*get_metadata)
+       (void *state, grpc_auth_metadata_context context,
+        grpc_credentials_plugin_metadata_cb cb, void *user_data);
+
+//type of the Haskell callback that we use to create auth metadata on the client
+typedef void haskell_get_metadata(grpc_auth_metadata_context*,
+                                  grpc_credentials_plugin_metadata_cb,
+                                  void*);
+
+grpc_metadata_credentials_plugin* mk_metadata_client_plugin(
+  haskell_get_metadata* f);
 
 #endif //GRPC_HASKELL

src/Network/GRPC/HighLevel.hs
@@ -1,10 +1,50 @@
 module Network.GRPC.HighLevel (
+
+-- * Types
+  MetadataMap(..)
+, MethodName(..)
+, StatusDetails(..)
+, StatusCode(..)
+, GRPCIOError(..)
+
 -- * Server
-Handler(..)
+, Handler(..)
 , ServerOptions(..)
 , defaultOptions
 , serverLoop
+, ServerCall(..)
+, serverCallCancel
+, serverCallIsExpired
+
+-- * Client
+, NormalRequestResult(..)
+, ClientCall
+, clientCallCancel
+
+-- * Client and Server Auth
+, AuthContext
+, AuthProperty(..)
+, getAuthProperties
+, addAuthProperty
+
+-- * Server Auth
+, ServerSSLConfig(..)
+, ProcessMeta
+, AuthProcessorResult(..)
+, SslClientCertificateRequestType(..)
+
+-- * Client Auth
+, ClientSSLConfig(..)
+, ClientSSLKeyCertPair(..)
+, ClientMetadataCreate
+, ClientMetadataCreateResult(..)
+, AuthMetadataContext(..)
+
+-- * Streaming utilities
+, StreamSend
+, StreamRecv
 )
  where
 
-import Network.GRPC.HighLevel.Server
+import           Network.GRPC.HighLevel.Server
+import           Network.GRPC.LowLevel

commit 09c56e661e7ef8b29d04f341836af0a28be20ab2

Fix examples broken by #68

examples/hellos/hellos-client/Main.hs
@@ -110,7 +110,7 @@ doHelloBi c n = do
 
 highlevelMain :: IO ()
 highlevelMain = withGRPC $ \g ->
-  withClient g (ClientConfig "localhost" 50051 [] Nothing) $ \c -> do
+  withClient g (ClientConfig "localhost" 50051 [] Nothing Nothing) $ \c -> do
     let n = 100000
     putStrLn "-------------- HelloSS --------------"
     doHelloSS c n

examples/tutorial/ArithmeticClient.hs
@@ -11,6 +11,7 @@ clientConfig = ClientConfig { clientServerHost = "localhost"
                             , clientServerPort = 50051
                             , clientArgs = []
                             , clientSSLConfig = Nothing
+                            , clientAuthority = Nothing
                             }
 
 main :: IO ()