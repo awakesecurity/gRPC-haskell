awakesecurity/gRPC-haskell

commit 3366dde7ab5fa88e12c006845d5a32da9d959467

Connor/security (#68)
* begin security bindings

* secure channel creation

* SSL credentials support

* add client-side ssl support

* ssl test

* read file before passing to channel credentials, free credentials when done creating

* use localhost key/cert for test

* WIP sketch of callbacks for metadata plugins

* conversion from Haskell auth processor to C

* add test for custom server metadata auth processor

* wip auth failure test

* rebase tweak

* working test of custom auth metadata server processor

* improve security docs, clean up

* add unsafe layer client-side auth metadata plugin functionality

* add client config option for custom auth metadata

* WIP client-side metadata auth plugin. Crashing when calling C callback.

* get initial version of client-side metadata plugins working

* replace String with ByteString in a few places, add function for getting AuthProperty

* AuthContext utilities and more documentation

* end-to-end test of client and server auth plugins

* remove redundant tests

* function for parents in unary calls, add deactivated failing test for auth metadata propagation from parent to child

* some cleanup

* tweaks

* more tweaks

* remove unused file

* docs tweak

* consolidate exports

* update protobuf-wire commit

examples/hellos/hellos-client/Main.hs
@@ -110,7 +110,7 @@ doHelloBi c n = do
 
 highlevelMain :: IO ()
 highlevelMain = withGRPC $ \g ->
-  withClient g (ClientConfig "localhost" 50051 []) $ \c -> do
+  withClient g (ClientConfig "localhost" 50051 [] Nothing) $ \c -> do
     let n = 100000
     putStrLn "-------------- HelloSS --------------"
     doHelloSS c n

grpc-haskell.cabal
@@ -55,6 +55,7 @@ library
     Network.GRPC.Unsafe.Metadata
     Network.GRPC.Unsafe.Op
     Network.GRPC.Unsafe
+    Network.GRPC.Unsafe.Security
     Network.GRPC.LowLevel
     Network.GRPC.LowLevel.Server.Unregistered
     Network.GRPC.LowLevel.Client.Unregistered

examples/echo/echo-client/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists   #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 {-# OPTIONS_GHC -fno-warn-unused-binds       #-}
 
@@ -22,7 +23,7 @@ addMethod = MethodName "/echo.Add/DoAdd"
 _unregistered c = U.clientRequest c echoMethod 1 "hi" mempty
 
 regMain = withGRPC $ \g ->
-  withClient g (ClientConfig "localhost" 50051 []) $ \c -> do
+  withClient g (ClientConfig "localhost" 50051 [] Nothing) $ \c -> do
   rm <- clientRegisterMethodNormal c echoMethod
   replicateM_ 100000 $ clientRequest c rm 5 "hi" mempty >>= \case
     Left e -> fail $ "Got client error: " ++ show e
@@ -42,7 +43,7 @@ instance Message AddResponse
 -- TODO: Create Network.GRPC.HighLevel.Client w/ request variants
 
 highlevelMain = withGRPC $ \g ->
-    withClient g (ClientConfig "localhost" 50051 []) $ \c -> do
+    withClient g (ClientConfig "localhost" 50051 [] Nothing) $ \c -> do
     rm <- clientRegisterMethodNormal c echoMethod
     rmAdd <- clientRegisterMethodNormal c addMethod
     let oneThread = replicateM_ 10000 $ body c rm rmAdd
@@ -71,4 +72,5 @@ highlevelMain = withGRPC $ \g ->
                     | dec == AddResponse (x + y) -> return ()
                     | otherwise -> fail $ "Got wrong add answer: " ++ show dec ++ "expected: " ++ show x ++ " + " ++ show y ++ " = " ++ show (x+y)
 
+main :: IO ()
 main = highlevelMain

examples/echo/echo-server/Main.hs
@@ -20,6 +20,7 @@ import qualified Network.GRPC.HighLevel.Server.Unregistered as U
 import           Network.GRPC.LowLevel
 import qualified Network.GRPC.LowLevel.Call.Unregistered   as U
 import qualified Network.GRPC.LowLevel.Server.Unregistered as U
+import           Network.GRPC.Unsafe.Security as U
 
 serverMeta :: MetadataMap
 serverMeta = [("test_meta", "test_meta_value")]
@@ -123,4 +124,4 @@ main :: IO ()
 main = highlevelMainUnregistered
 
 defConfig :: ServerConfig
-defConfig = ServerConfig "localhost" 50051 [] [] [] [] []
+defConfig = ServerConfig "localhost" 50051 [] [] [] [] [] Nothing

examples/hellos/hellos-server/Main.hs
@@ -80,4 +80,4 @@ main :: IO ()
 main = highlevelMainUnregistered
 
 defConfig :: ServerConfig
-defConfig = ServerConfig "localhost" 50051 [] [] [] [] []
+defConfig = ServerConfig "localhost" 50051 [] [] [] [] [] Nothing

src/Network/GRPC/LowLevel/Client.hs
@@ -16,6 +16,7 @@ import           Control.Concurrent.MVar
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Except
+import qualified Data.ByteString                       as B
 import           Data.ByteString                       (ByteString)
 import           Network.GRPC.LowLevel.Call
 import           Network.GRPC.LowLevel.CompletionQueue
@@ -25,6 +26,7 @@ import qualified Network.GRPC.Unsafe                   as C
 import qualified Network.GRPC.Unsafe.ChannelArgs       as C
 import qualified Network.GRPC.Unsafe.Constants         as C
 import qualified Network.GRPC.Unsafe.Op                as C
+import qualified Network.GRPC.Unsafe.Security          as C
 import qualified Network.GRPC.Unsafe.Time              as C
 
 -- | Represents the context needed to perform client-side gRPC operations.
@@ -33,25 +35,72 @@ data Client = Client {clientChannel :: C.Channel,
                       clientConfig  :: ClientConfig
                      }
 
+data ClientSSLKeyCertPair = ClientSSLKeyCertPair
+  {clientPrivateKey :: FilePath,
+   clientCert :: FilePath}
+
+-- | SSL configuration for the client. It's perfectly acceptable for both fields
+-- to be 'Nothing', in which case default fallbacks will be used for the server
+-- root cert.
+data ClientSSLConfig = ClientSSLConfig
+  {serverRootCert :: Maybe FilePath,
+   -- ^ Path to the server root certificate. If 'Nothing', gRPC will attempt to
+   -- fall back to a default.
+   clientSSLKeyCertPair :: Maybe ClientSSLKeyCertPair,
+   -- ^ The client's private key and cert, if available.
+   clientMetadataPlugin :: Maybe C.ClientMetadataCreate
+   -- ^ Optional plugin for attaching additional metadata to each call.
+  }
+
 -- | Configuration necessary to set up a client.
 
 data ClientConfig = ClientConfig {serverHost :: Host,
                                   serverPort :: Port,
-                                  clientArgs :: [C.Arg]
+                                  clientArgs :: [C.Arg],
                                   -- ^ Optional arguments for setting up the
                                   -- channel on the client. Supplying an empty
                                   -- list will cause the channel to use gRPC's
                                   -- default options.
+                                  clientSSLConfig :: Maybe ClientSSLConfig
+                                  -- ^ If 'Nothing', the client will use an
+                                  -- insecure connection to the server.
+                                  -- Otherwise, will use the supplied config to
+                                  -- connect using SSL.
                                  }
 
 clientEndpoint :: ClientConfig -> Endpoint
 clientEndpoint ClientConfig{..} = endpoint serverHost serverPort
 
+addMetadataCreds :: C.ChannelCredentials
+                    -> Maybe C.ClientMetadataCreate
+                    -> IO C.ChannelCredentials
+addMetadataCreds c Nothing = return c
+addMetadataCreds c (Just create) = do
+  callCreds <- C.createCustomCallCredentials create
+  C.compositeChannelCredentialsCreate c callCreds C.reserved
+
+createChannel :: ClientConfig -> C.GrpcChannelArgs -> IO C.Channel
+createChannel conf@ClientConfig{..} chanargs =
+  case clientSSLConfig of
+    Nothing -> C.grpcInsecureChannelCreate e chanargs C.reserved
+    Just (ClientSSLConfig rootCertPath Nothing plugin) ->
+      do rootCert <- mapM B.readFile rootCertPath
+         C.withChannelCredentials rootCert Nothing Nothing $ \creds -> do
+           creds' <- addMetadataCreds creds plugin
+           C.secureChannelCreate creds' e chanargs C.reserved
+    Just (ClientSSLConfig x (Just (ClientSSLKeyCertPair y z)) plugin) ->
+      do rootCert <- mapM B.readFile x
+         privKey <- Just <$> B.readFile y
+         clientCert <- Just <$> B.readFile z
+         C.withChannelCredentials rootCert privKey clientCert $ \creds -> do
+           creds' <- addMetadataCreds creds plugin
+           C.secureChannelCreate creds' e chanargs C.reserved
+  where (Endpoint e) = clientEndpoint conf
+
 createClient :: GRPC -> ClientConfig -> IO Client
-createClient grpc clientConfig@ClientConfig{..} =
-  C.withChannelArgs clientArgs $ \chanargs -> do
-    let Endpoint e = clientEndpoint clientConfig
-    clientChannel <- C.grpcInsecureChannelCreate e chanargs C.reserved
+createClient grpc clientConfig =
+  C.withChannelArgs (clientArgs clientConfig) $ \chanargs -> do
+    clientChannel <- createChannel clientConfig chanargs
     clientCQ <- createCompletionQueue grpc
     return Client{..}
 
@@ -162,7 +211,7 @@ withClientCall cl rm tm = withClientCallParent cl rm tm Nothing
 withClientCallParent :: Client
                      -> RegisteredMethod mt
                      -> TimeoutSeconds
-                     -> Maybe (ServerCall a)
+                     -> Maybe (ServerCall b)
                         -- ^ Optional parent call for cascading cancellation
                      -> (ClientCall -> IO (Either GRPCIOError a))
                      -> IO (Either GRPCIOError a)
@@ -359,16 +408,33 @@ clientRW' (clientCQ -> cq) (unsafeCC -> c) initMeta f = runExceptT $ do
 
 -- | Make a request of the given method with the given body. Returns the
 -- server's response.
-clientRequest :: Client
-              -> RegisteredMethod 'Normal
-              -> TimeoutSeconds
-              -> ByteString
-              -- ^ The body of the request
-              -> MetadataMap
-              -- ^ Metadata to send with the request
-              -> IO (Either GRPCIOError NormalRequestResult)
-clientRequest cl@(clientCQ -> cq) rm tm body initMeta =
-  withClientCall cl rm tm (fmap join . go)
+clientRequest
+  :: Client
+  -> RegisteredMethod 'Normal
+  -> TimeoutSeconds
+  -> ByteString
+  -- ^ The body of the request
+  -> MetadataMap
+  -- ^ Metadata to send with the request
+  -> IO (Either GRPCIOError NormalRequestResult)
+clientRequest c = clientRequestParent c Nothing
+
+-- | Like 'clientRequest', but allows the user to supply an optional parent
+-- call, so that call cancellation can be propagated from the parent to the
+-- child. This is intended for servers that call other servers.
+clientRequestParent
+  :: Client
+  -> Maybe (ServerCall a)
+  -- ^ optional parent call
+  -> RegisteredMethod 'Normal
+  -> TimeoutSeconds
+  -> ByteString
+  -- ^ The body of the request
+  -> MetadataMap
+  -- ^ Metadata to send with the request
+  -> IO (Either GRPCIOError NormalRequestResult)
+clientRequestParent cl@(clientCQ -> cq) p rm tm body initMeta =
+  withClientCallParent cl rm tm p (fmap join . go)
   where
     go (unsafeCC -> c) =
       -- NB: the send and receive operations below *must* be in separate

cbits/grpc_haskell.c
@@ -1,6 +1,7 @@
 #include <grpc/grpc.h>
 #include <grpc/byte_buffer.h>
 #include <grpc/byte_buffer_reader.h>
+#include <grpc/grpc_security.h>
 #include <grpc/impl/codegen/grpc_types.h>
 #include <grpc/impl/codegen/compression_types.h>
 #include <stdio.h>
@@ -158,7 +159,7 @@ void metadata_array_destroy(grpc_metadata_array **arr){
 }
 
 grpc_metadata* metadata_alloc(size_t n){
-  grpc_metadata *retval = malloc(sizeof(grpc_metadata)*n);
+  grpc_metadata *retval = calloc(n,sizeof(grpc_metadata));
   return retval;
 }
 
@@ -462,4 +463,86 @@ void destroy_arg_array(grpc_arg* args, size_t n){
     }
   }
   free(args);
+}
+
+grpc_auth_property_iterator* grpc_auth_context_property_iterator_(
+  const grpc_auth_context* ctx){
+
+  grpc_auth_property_iterator* i = malloc(sizeof(grpc_auth_property_iterator));
+  *i = grpc_auth_context_property_iterator(ctx);
+  return i;
+}
+
+grpc_server_credentials* ssl_server_credentials_create_internal(
+  const char* pem_root_certs, const char* pem_key, const char* pem_cert,
+  grpc_ssl_client_certificate_request_type force_client_auth){
+
+  grpc_ssl_pem_key_cert_pair pair = {pem_key, pem_cert};
+  grpc_server_credentials* creds = grpc_ssl_server_credentials_create_ex(
+    pem_root_certs, &pair, 1, force_client_auth, NULL);
+  return creds;
+}
+
+grpc_channel_credentials* grpc_ssl_credentials_create_internal(
+  const char* pem_root_certs, const char* pem_key, const char* pem_cert){
+
+  grpc_channel_credentials* creds;
+  if(pem_key && pem_cert){
+    grpc_ssl_pem_key_cert_pair pair = {pem_key, pem_cert};
+    creds = grpc_ssl_credentials_create(pem_root_certs, &pair, NULL);
+  }
+  else{
+    creds = grpc_ssl_credentials_create(pem_root_certs, NULL, NULL);
+  }
+  return creds;
+}
+
+void grpc_server_credentials_set_auth_metadata_processor_(
+  grpc_server_credentials* creds, grpc_auth_metadata_processor* p){
+
+  grpc_server_credentials_set_auth_metadata_processor(creds, *p);
+}
+
+grpc_auth_metadata_processor* mk_auth_metadata_processor(
+  void (*process)(void *state, grpc_auth_context *context,
+                  const grpc_metadata *md, size_t num_md,
+                  grpc_process_auth_metadata_done_cb cb, void *user_data)){
+
+  //TODO: figure out when to free this.
+  grpc_auth_metadata_processor* p = malloc(sizeof(grpc_auth_metadata_processor));
+  p->process = process;
+  p->destroy = NULL;
+  p->state = NULL;
+  return p;
+}
+
+grpc_call_credentials* grpc_metadata_credentials_create_from_plugin_(
+  grpc_metadata_credentials_plugin* plugin){
+
+  return grpc_metadata_credentials_create_from_plugin(*plugin, NULL);
+}
+
+//This is a hack to work around GHC being unable to deal with raw struct params.
+//This callback is registered as the get_metadata callback for the call, and its
+//only job is to cast the void* state pointer to the correct function pointer
+//type and call the Haskell function with it.
+void metadata_dispatcher(void *state, grpc_auth_metadata_context context,
+ grpc_credentials_plugin_metadata_cb cb, void *user_data){
+
+  ((haskell_get_metadata*)state)(&context, cb, user_data);
+}
+
+grpc_metadata_credentials_plugin* mk_metadata_client_plugin(
+  haskell_get_metadata* f){
+
+  //TODO: figure out when to free this.
+  grpc_metadata_credentials_plugin* p =
+    malloc(sizeof(grpc_metadata_credentials_plugin));
+
+  p->get_metadata = metadata_dispatcher;
+  p->destroy = NULL;
+  p->state = f;
+  p->type = "grpc-haskell custom credentials";
+
+  return p;
 }

include/grpc_haskell.h
@@ -2,6 +2,7 @@
 #define GRPC_HASKELL
 
 #include <grpc/grpc.h>
+#include <grpc/grpc_security.h>
 #include <grpc/impl/codegen/slice.h>
 #include <grpc/impl/codegen/time.h>
 #include <grpc/byte_buffer.h>
@@ -163,5 +164,41 @@ void create_int_arg(grpc_arg* args, size_t i,
                     enum supported_arg_key key, int value);
 
 void destroy_arg_array(grpc_arg* args, size_t n);
+
+grpc_auth_property_iterator* grpc_auth_context_property_iterator_(
+  const grpc_auth_context* ctx);
+
+grpc_server_credentials* ssl_server_credentials_create_internal(
+  const char* pem_root_certs, const char* pem_key, const char* pem_cert,
+  grpc_ssl_client_certificate_request_type force_client_auth);
+
+grpc_channel_credentials* grpc_ssl_credentials_create_internal(
+  const char* pem_root_certs, const char* pem_key, const char* pem_cert);
+
+void grpc_server_credentials_set_auth_metadata_processor_(
+  grpc_server_credentials* creds, grpc_auth_metadata_processor* p);
+
+//packs a Haskell server-side auth processor function pointer into the
+//appropriate struct expected by gRPC.
+grpc_auth_metadata_processor* mk_auth_metadata_processor(
+  void (*process)(void *state, grpc_auth_context *context,
+                  const grpc_metadata *md, size_t num_md,
+                  grpc_process_auth_metadata_done_cb cb, void *user_data));
+
+grpc_call_credentials* grpc_metadata_credentials_create_from_plugin_(
+  grpc_metadata_credentials_plugin* plugin);
+
+//type of the callback used to create auth metadata on the client
+typedef void (*get_metadata)
+       (void *state, grpc_auth_metadata_context context,
+        grpc_credentials_plugin_metadata_cb cb, void *user_data);
+
+//type of the Haskell callback that we use to create auth metadata on the client
+typedef void haskell_get_metadata(grpc_auth_metadata_context*,
+                                  grpc_credentials_plugin_metadata_cb,
+                                  void*);
+
+grpc_metadata_credentials_plugin* mk_metadata_client_plugin(
+  haskell_get_metadata* f);
 
 #endif //GRPC_HASKELL

commit 28288a17b7fd87b194773207e4bd6554c67642c8

Client helpers, re-exports, and fixups (#70)
* Derive `Show` instance for `ClientSSLKeyCertPair`

* Add a couple client helper functions & add additional exports

* Add `grpc-haskell-core` target; add `c2hs` dep

* Fix examples broken by #68

* rm `build-tools` and `include-dirs` directives from toplevel `.cabal`

* More `ClientConfig` fixes

* Ensure examples are built

bench/Bench.hs
@@ -97,7 +97,7 @@ serverOpts =
 
 main :: IO ()
 main = bracket startServer stopServer $ const $ withGRPC $ \grpc ->
-  withClient grpc (ClientConfig "localhost" 50051 [] Nothing) $ \c -> do
+  withClient grpc (ClientConfig "localhost" 50051 [] Nothing Nothing) $ \c -> do
     rmAdd <- clientRegisterMethodNormal c addMethod
     rmClientStream <- clientRegisterMethodClientStreaming c addClientStreamMethod
     rmServerStream <- clientRegisterMethodServerStreaming c addServerStreamMethod

core/src/Network/GRPC/LowLevel/Client.hs
@@ -37,8 +37,9 @@ data Client = Client {clientChannel :: C.Channel,
                      }
 
 data ClientSSLKeyCertPair = ClientSSLKeyCertPair
-  {clientPrivateKey :: FilePath,
-   clientCert :: FilePath}
+  { clientPrivateKey :: FilePath,
+    clientCert       :: FilePath
+  } deriving Show
 
 -- | SSL configuration for the client. It's perfectly acceptable for both fields
 -- to be 'Nothing', in which case default fallbacks will be used for the server

examples/echo/echo-hs/EchoClient.hs
@@ -36,7 +36,7 @@ main = do
     cfg      = ClientConfig
                  (Host . fromMaybe "localhost" . unHelpful $ bind)
                  (Port . fromMaybe 50051       . unHelpful $ port)
-                 [] Nothing
+                 [] Nothing Nothing
   withGRPC $ \g -> withClient g cfg $ \c -> do
     Echo{..} <- echoClient c
     echoDoEcho (ClientNormalRequest rqt 5 mempty) >>= \case

examples/hellos/hellos-client/Main.hs
@@ -110,7 +110,7 @@ doHelloBi c n = do
 
 highlevelMain :: IO ()
 highlevelMain = withGRPC $ \g ->
-  withClient g (ClientConfig "localhost" 50051 [] Nothing) $ \c -> do
+  withClient g (ClientConfig "localhost" 50051 [] Nothing Nothing) $ \c -> do
     let n = 100000
     putStrLn "-------------- HelloSS --------------"
     doHelloSS c n

examples/tutorial/ArithmeticClient.hs
@@ -11,6 +11,7 @@ clientConfig = ClientConfig { clientServerHost = "localhost"
                             , clientServerPort = 50051
                             , clientArgs = []
                             , clientSSLConfig = Nothing
+                            , clientAuthority = Nothing
                             }
 
 main :: IO ()

grpc-haskell.cabal
@@ -30,6 +30,7 @@ library
     , grpc-haskell-core
 
     , async ==2.1.*
+    , managed >= 1.0.5
 
   exposed-modules:
     Network.GRPC.HighLevel
@@ -37,10 +38,8 @@ library
     Network.GRPC.HighLevel.Server
     Network.GRPC.HighLevel.Server.Unregistered
     Network.GRPC.HighLevel.Client
-  build-tools:          c2hs
   default-language:     Haskell2010
   ghc-options:          -Wall -fwarn-incomplete-patterns -fno-warn-unused-do-bind
-  include-dirs:         include
   hs-source-dirs:       src
   default-extensions:   CPP
   CC-Options: -std=c99

release.nix
@@ -213,7 +213,10 @@ let
               (haskellPackagesNew.callPackage ./nix/proto3-suite.nix {});
 
           grpc-haskell-core =
-            usesGRPC (haskellPackagesNew.callPackage ./core { });
+            usesGRPC
+              (pkgs.haskell.lib.overrideCabal
+                (haskellPackagesNew.callPackage ./core { })
+                (_: { buildDepends = [ haskellPackagesNew.c2hs ]; }));
 
           grpc-haskell-no-tests =
             usesGRPC
@@ -242,11 +245,18 @@ let
                     ]);
 
                   in rec {
+                    configureFlags = (oldDerivation.configureFlags or []) ++ [
+                      "--flags=with-examples"
+                    ];
+
                     buildDepends = [
                       pkgs.makeWrapper
                       # Give our nix-shell its own cabal so we don't pick up one
                       # from the user's environment by accident.
                       haskellPackagesNew.cabal-install
+
+                      # And likewise for c2hs
+                      haskellPackagesNew.c2hs
                     ];
 
                     patches = [ tests/tests.patch ];
@@ -299,11 +309,18 @@ let
         pkgs = import nixpkgs { inherit config; };
 
 in
-  { grpc-haskell-linux    =  linuxPkgs.haskellPackages.grpc-haskell;
-    grpc-haskell-darwin   = darwinPkgs.haskellPackages.grpc-haskell;
-    grpc-haskell          =       pkgs.haskellPackages.grpc-haskell;
-    grpc-haskell-no-tests = pkgs.haskellPackages.grpc-haskell-no-tests;
-    grpc-linux            =  linuxPkgs.grpc;
-    grpc-darwin           = darwinPkgs.grpc;
-    grpc                  =       pkgs.grpc;
+  {
+    grpc-haskell-core-linux    =  linuxPkgs.haskellPackages.grpc-haskell-core;
+    grpc-haskell-core-darwin   = darwinPkgs.haskellPackages.grpc-haskell-core;
+    grpc-haskell-core          =       pkgs.haskellPackages.grpc-haskell-core;
+
+    grpc-haskell-linux         =  linuxPkgs.haskellPackages.grpc-haskell;
+    grpc-haskell-darwin        = darwinPkgs.haskellPackages.grpc-haskell;
+    grpc-haskell               =       pkgs.haskellPackages.grpc-haskell;
+    grpc-haskell-no-tests      =       pkgs.haskellPackages.grpc-haskell-no-tests;
+
+    grpc-linux                 =  linuxPkgs.grpc;
+    grpc-darwin                = darwinPkgs.grpc;
+
+    grpc                       =       pkgs.grpc;
   }

src/Network/GRPC/HighLevel/Client.hs
@@ -1,44 +1,58 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
 
 module Network.GRPC.HighLevel.Client
-  ( RegisteredMethod
-  , TimeoutSeconds
-  , MetadataMap(..)
-  , StatusDetails(..)
-  , GRPCMethodType(..)
-  , StreamRecv
-  , StreamSend
-  , WritesDone
-  , LL.Client
-  , ServiceClient
-  , ClientError(..)
+  ( ClientError(..)
+  , ClientRegisterable(..)
   , ClientRequest(..)
   , ClientResult(..)
-  , ClientRegisterable(..)
+  , GRPCMethodType(..)
+  , MetadataMap(..)
+  , RegisteredMethod
+  , ServiceClient
+  , StatusCode(..)
+  , StatusDetails(..)
+  , StreamRecv
+  , StreamSend
+  , TimeoutSeconds
+  , WritesDone
+
+  , LL.Client
+  , LL.ClientConfig(..)
+  , LL.ClientSSLConfig(..)
+  , LL.ClientSSLKeyCertPair(..)
+  , LL.Host(..)
+  , LL.Port(..)
+
   , clientRequest
+
+  -- * Client utility functions
+  , acquireClient
+  , simplifyUnary
   )
 
 where
 
-import qualified Network.GRPC.LowLevel.Client as LL
-import qualified Network.GRPC.LowLevel.Call as LL
-import Network.GRPC.LowLevel.CompletionQueue (TimeoutSeconds)
-import Network.GRPC.LowLevel ( GRPCMethodType(..)
-                             , StatusCode(..)
-                             , StatusDetails(..)
-                             , MetadataMap(..)
-                             , GRPCIOError(..)
-                             , StreamRecv
-                             , StreamSend )
-import Network.GRPC.LowLevel.Op (WritesDone)
-import Network.GRPC.HighLevel.Server (convertRecv, convertSend)
-
-import Proto3.Suite (Message, toLazyByteString, fromByteString)
-import Proto3.Wire.Decode (ParseError)
-import qualified Data.ByteString.Lazy as BL
+import           Control.Monad.Managed                 (Managed, liftIO,
+                                                        managed)
+import qualified Data.ByteString.Lazy                  as BL
+import           Network.GRPC.HighLevel.Server         (convertRecv,
+                                                        convertSend)
+import           Network.GRPC.LowLevel                 (GRPCIOError (..),
+                                                        GRPCMethodType (..),
+                                                        MetadataMap (..),
+                                                        StatusCode (..),
+                                                        StatusDetails (..),
+                                                        StreamRecv, StreamSend)
+import qualified Network.GRPC.LowLevel                 as LL
+import           Network.GRPC.LowLevel.CompletionQueue (TimeoutSeconds)
+import           Network.GRPC.LowLevel.Op              (WritesDone)
+import           Proto3.Suite                          (Message, fromByteString,
+                                                        toLazyByteString)
+import           Proto3.Wire.Decode                    (ParseError)
 
 newtype RegisteredMethod (mt :: GRPCMethodType) request response
   = RegisteredMethod (LL.RegisteredMethod mt)
@@ -121,3 +135,45 @@ clientRequest client (RegisteredMethod method) (ClientBiDiRequest timeout meta h
     mkResponse (Left ioError_) = ClientErrorResponse (ClientIOError ioError_)
     mkResponse (Right (meta_, rspCode_, details_)) =
       ClientBiDiResponse meta_ rspCode_ details_
+
+acquireClient
+  :: LL.ClientConfig
+     -- ^ The client configuration (host, port, SSL settings, etc)
+  -> (LL.Client -> IO (ServiceClient service))
+     -- ^ The client implementation (typically generated)
+  -> Managed (ServiceClient service)
+acquireClient cfg impl = do
+  g <- managed LL.withGRPC
+  c <- managed (LL.withClient g cfg)
+  liftIO (impl c)
+
+-- | A utility for simplifying gRPC client requests in common cases; you can use
+-- this to avoid 'ClientRequest' and 'ClientResponse' pattern-matching
+-- boilerplate at call sites.
+simplifyUnary :: TimeoutSeconds
+                -- ^ RPC call timeout, in seconds
+              -> MetadataMap
+                 -- ^ RPC call metadata
+              -> (ClientError -> IO (b, StatusDetails))
+                 -- ^ Handler for client errors
+              -> (b -> StatusCode -> StatusDetails -> IO (b, StatusDetails))
+                 -- ^ Handler for non-StatusOK responses
+              -> (ClientRequest 'Normal a b -> IO (ClientResult 'Normal b))
+                 -- ^ gRPC function implementation (typically generated by gRPC-haskell)
+              -> (a -> IO (b, StatusDetails))
+                 -- ^ The simplified happy-path (StatusOk) unary call action
+simplifyUnary timeout meta clientError nonStatusOkError f x = do
+
+  let request = ClientNormalRequest x timeout meta
+
+  response <- f request
+
+  case response of
+    ClientNormalResponse y _ _ StatusOk details
+      -> pure (y, details)
+
+    ClientNormalResponse y _ _ code details
+      -> nonStatusOkError y code details
+
+    ClientErrorResponse err
+      -> clientError err