c1809e4c528411389fb0929ab58c2c4ce4eabfb3
Joel Stanley <joel@awakesecurity.com>
2021-06-30 17:40:32 -0500

    Add some helper functions for MetadataMap lookup; documentation

--- core/src/Network/GRPC/LowLevel/GRPC/MetadataMap.hs
+++ core/src/Network/GRPC/LowLevel/GRPC/MetadataMap.hs
@@ -8,13 +8,27 @@ import Data.Function (on)
 import GHC.Exts (IsList(..))
 import Data.List (sortBy, groupBy)
 import Data.Ord (comparing)
+import qualified Data.List.NonEmpty as NE
 import qualified Data.Map.Strict as M
 
--- | Represents metadata for a given RPC, consisting of key-value pairs. Keys
--- are allowed to be repeated, with the `last` element of value list usually
--- taken as the final value for that key. Since repeated keys are unlikely in
--- practice, the 'IsList' instance uses key-value pairs as items. For example,
--- @fromList [("key1","val1"),("key2","val2"),("key1","val3")]@.
+{- | Represents metadata for a given RPC, consisting of key-value pairs (often
+   referred to as "GRPC custom metadata headers").
+
+   Keys are allowed to be repeated, with the 'last' value element (i.e., the
+   last-presented) usually taken as the value for that key (see 'lookupLast' and
+   'lookupAll').
+
+   Since repeated keys are unlikely in practice, the 'IsList' instance for
+   'MetadataMap' uses key-value pairs as items, and treats duplicates
+   appropriately.
+
+   >>> lookupAll "k1" (fromList [("k1","x"), ("k2", "z"), ("k1", "y")])
+   Just ("x" :| ["y"])
+
+   >>> lookupLast "k1" (fromList [("k1","x"), ("k2", "z"), ("k1", "y")])
+   Just "y"
+-}
+
 newtype MetadataMap = MetadataMap {unMap :: M.Map ByteString [ByteString]}
   deriving Eq
 
@@ -39,3 +53,11 @@ instance IsList MetadataMap where
            . map (fmap toList)
            . M.toList
            . unMap
+
+-- | Obtain all header values for a given header key, in presentation order.
+lookupAll :: ByteString -> MetadataMap -> Maybe (NE.NonEmpty ByteString)
+lookupAll k (MetadataMap md) = NE.nonEmpty =<< M.lookup k md
+
+-- | Obtain the last-presented header value for a given header key.
+lookupLast :: ByteString -> MetadataMap -> Maybe ByteString
+lookupLast k = fmap NE.last . lookupAll k
