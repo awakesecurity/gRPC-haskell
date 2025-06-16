4c832b0a0e5283f4061ccef213026e61a847ce62
Tomas Drtina <tdrtina-ext@arista.com>
2025-06-16 17:14:39 +0200

    typo in nixpkgs date

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -7,7 +7,7 @@
 # The SHA256 will be printed as the last line of stdout.
 
 import (builtins.fetchTarball {
-    # "nixos-unstable" as on 2024-04-18
+    # "nixos-unstable" as on 2025-04-18
     url    = "https://github.com/NixOS/nixpkgs/archive/b024ced1aac25639f8ca8fdfc2f8c4fbd66c48ef.tar.gz";
     sha256 = "09dahi81cn02gnzsc8a00n945dxc18656ar0ffx5vgxjj1nhgsvy";
   })
