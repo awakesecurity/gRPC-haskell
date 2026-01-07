5d5c40d26729b91fc6029b40772262d32b75bfa0
Tomas Drtina <tdrtina-ext@arista.com>
2026-01-07 08:15:41 -0800

    haskellPackages.vcr: don't test on Darwin

--- release.nix
+++ release.nix
@@ -70,6 +70,10 @@ let
 
     haskellPackages = pkgsOld.haskell.packages.${ghc}.override {
       overrides = haskellPackagesNew: haskellPackagesOld: {
+        vcr = pkgsNew.lib.pipe haskellPackagesOld.vcr [
+          (pkgsNew.haskell.lib.compose.dontCheckIf pkgsNew.stdenv.hostPlatform.isDarwin)
+        ];
+
         proto3-wire =
           haskellPackagesNew.callPackage ./nix/proto3-wire.nix { };
 
@@ -211,7 +215,7 @@ let
 
    nixpkgs = import ./nixpkgs.nix;
    linuxPkgs = nixpkgs { inherit config overlays; system = "x86_64-linux" ; };
-  darwinPkgs = nixpkgs { inherit config overlays; system = "x86_64-darwin"; };
+  darwinPkgs = nixpkgs { inherit config overlays; system = "aarch64-darwin"; };
         pkgs = nixpkgs { inherit config overlays; };
 
 in
