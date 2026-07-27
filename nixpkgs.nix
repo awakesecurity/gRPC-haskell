f0796143c9d9ab914b50637f29734a2f925568c6
Tomas Drtina <tm.drtina@gmail.com>
2026-07-27 03:26:01 -0700

    nixpkgs: update pin

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -6,8 +6,8 @@
 #
 # The SHA256 will be printed as the last line of stdout.
 
-import (builtins.fetchTarball {
-    # "nixos-unstable" as on 2026-01-06
-    url    = "https://github.com/NixOS/nixpkgs/archive/9f0c42f8bc7151b8e7e5840fb3bd454ad850d8c5.tar.gz";
-    sha256 = "sha256-UWYqmD7JFBEDBHWYcqE6s6c77pWdcU/i+bwD6XxMb8A=";
+import (fetchTarball {
+    # "nixos-unstable" as on 2026-07-26
+    url    = "https://github.com/NixOS/nixpkgs/archive/624af665418d3c65d544145b4d34ad696439570e.tar.gz";
+    sha256 = "sha256-m0pDuRJG7EDo9ri+4Ksu83VsI+PlxNC9lNBfydejce4=";
   })
